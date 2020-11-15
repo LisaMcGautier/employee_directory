308e0b4613148a891a93a3cbcbd997a77440494e
Lisa McDonald <nixxxxer@aol.com>
2020-11-15 11:32:56 -0500

    update style

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,8 @@ const Header = () => (
             <h1 className="display-4">Employee Directory</h1>
         </div>
 
+        Employee Directory returns a list of 50 employees, with images and contact information.
         <ul>
-            <li>Employee Directory returns a list of 50 employees, with images and contact information.</li>
             <li>Sort employees by first name or last name at the click of a button.</li>
             <li>Slice button returns a list of 25 employees.</li>
             <li>Refresh the page to return a new list of 50 employees.</li> 
